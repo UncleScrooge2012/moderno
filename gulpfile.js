9d23af38f393f14e62a01c22b7dcb461ebbe7fe8
UncleScrooge2012 <naruk2024@gmail.com>
2020-01-04 11:37:46 +0300

    add section products

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,9 @@ gulp.task('script',function(){
         'node_modules/wow.js/dist/wow.js',
         'node_modules/swiper/js/swiper.js',
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/mixitup/dist/mixitup.js'
+
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
